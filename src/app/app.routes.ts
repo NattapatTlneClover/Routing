65d112c0a03491e5d497083528ff2527ab052327
Nattapat Sukcharoenpohn <Tlneclover@gmail.com>
2024-10-30 18:39:52 +0700

    Splitting Route Definitions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import { routes as userRoutes } from './users/users.routes';
 
 export const routes: Routes = [
   {
@@ -8,7 +11,12 @@ export const routes: Routes = [
     component: NoTaskComponent,
   },
   {
-    path: 'users/:userId',  // <your-domain>/users/<uid>
+    path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
+    children: userRoutes,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
